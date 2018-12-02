9d516b67ce628358b32942cc05160a29fb7d8052
alexandrugris <alexandru.gris2006@gmail.com>
2018-12-02 21:21:59 +0200

    Added optional regularization term

--- team_ranking.py
+++ team_ranking.py
@@ -25,29 +25,26 @@ def next_idx():
 
 df = pd.DataFrame(columns=['Team 1', 'Team 2', 'Result'])
 
+teams = ['A', 'B', 'C', 'D', 'E', 'F' , 'G']
+
 df.loc[next_idx()] =  ['A', 'B', 1]
 df.loc[next_idx()] =  ['A', 'C', 1]
-df.loc[next_idx()] =  ['B', 'C', 0]
-df.loc[next_idx()] =  ['B', 'D', 0]
-df.loc[next_idx()] =  ['A', 'D', 0]
-df.loc[next_idx()] =  ['C', 'B', 0]
-df.loc[next_idx()] =  ['A', 'D', 1]
-df.loc[next_idx()] =  ['D', 'A', 0]
-df.loc[next_idx()] =  ['E', 'D', 1]
-df.loc[next_idx()] =  ['A', 'E', 1]
+df.loc[next_idx()] =  ['B', 'D', 1]
+df.loc[next_idx()] =  ['B', 'E', 1]
+df.loc[next_idx()] =  ['C', 'E', 1]
+df.loc[next_idx()] =  ['C', 'D', 1]
+df.loc[next_idx()] =  ['D', 'F', 1]
+df.loc[next_idx()] =  ['E', 'F', 1]
+df.loc[next_idx()] =  ['G', 'D', 1]
+df.loc[next_idx()] =  ['G', 'E', 1]
+df.loc[next_idx()] =  ['G', 'F', 1]
 
 
-params = pd.DataFrame.from_dict({
-        'A': 0.5,
-        'B' : 0.5,
-        'C' : 0.5,
-        'D' : 0.5,
-        'E' : 0.5,
-        }, orient='index', columns=['Value'])
+params = pd.DataFrame( [0.5] * len(teams), index = teams, columns = ['Value'] )
     
 def f(params):
     
-    params = pd.DataFrame(params, index=['A', 'B', 'C', 'D', 'E'])
+    params = pd.DataFrame(params, index=teams)
 
     df['Home'] = np.array(params.loc[df['Team 1']])
     df['Away'] = np.array(params.loc[df['Team 2']])
@@ -57,16 +54,21 @@ def f(params):
 
     df['Log'] = np.log(np.array(df['Rank'] * df['Result'] + (1.0 - df['Rank']) * (1.0 - df['Result']), dtype=float))
     
-    return -df['Log'].sum()
+    penalty = np.array(params)
+    penalty -= penalty.mean()
+    
+    # remove the np.sqrt ... to remove the penalty for large coefficients
+    # if you remove that, the teams that have not been beaten will have a max of 5 score 
+    return -df['Log'].sum() + 0.5 * np.sqrt(np.sum(penalty * penalty))
 
 from scipy.optimize import minimize, Bounds
 
-x = minimize(f, np.array(params), bounds=Bounds([1e-5, 1e-5, 1e-5, 1e-5, 1e-5], [5, 5, 5, 5, 5]))
+x = minimize(f, np.array(params), bounds=Bounds([1e-5] * len(teams), [5] * len(teams)))
 
 if x.success:
-   probs = pd.DataFrame(x.x, index=['A', 'B', 'C', 'D', 'E'])
+   probs = pd.DataFrame(x.x, index=teams)
 
 def prob(t1, t2):
     return probs.loc[t1] / (probs.loc[t1] + probs.loc[t2])
 
-prob('E', 'A')
+prob('G', 'A')
